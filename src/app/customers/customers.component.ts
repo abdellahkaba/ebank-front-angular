139bb57a450c8e672b5a7ef5bc29ad76b4a7d172
Abdellah <abdallahkaba98@gmail.com>
2024-03-05 06:34:57 +0000

    component-template-route

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrl: './customers.component.css'
+})
+export class CustomersComponent implements OnInit{
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
+}
