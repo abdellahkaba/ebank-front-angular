b7d141099dd4933671b62295c64fa75d502980c7
Abdellah <abdallahkaba98@gmail.com>
2024-03-06 12:08:30 +0000

    passage-param-customer-compte-route

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -3,6 +3,7 @@ import {CustomerService} from "../services/customer.service";
 import {catchError, map, Observable, throwError} from "rxjs";
 import {Customer} from "../model/cusotomer.model";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-customers',
@@ -16,11 +17,12 @@ export class CustomersComponent implements OnInit{
 
 
   /**
-   * On injecte le service CustomerService
+   * Injections des dependances
    * @param customerService
+   * @param fb
+   * @param router
    */
-
-  constructor(private customerService : CustomerService, private fb : FormBuilder) {
+  constructor(private customerService : CustomerService, private fb : FormBuilder, private router : Router) {
   }
   ngOnInit(): void {
     this.searchformGroup = this.fb.group({
@@ -63,5 +65,9 @@ export class CustomersComponent implements OnInit{
           console.log(err)
         }
       })
+  }
+
+  handleCustomerAccounts(customer: Customer) {
+      this.router.navigateByUrl("/customer-accounts/"+customer.id, {state :customer})
   }
 }
